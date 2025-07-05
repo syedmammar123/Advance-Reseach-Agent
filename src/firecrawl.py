d1117076dc9b2968d84cf2f33ea8c1eea2c4ac06
Syed Muhammad Ammar <syedmammar123@gmail.com>
2025-07-05 20:13:31 +0500

    first

---
+++ src/firecrawl.py
@@ -0,0 +1,34 @@
+import os
+from firecrawl import ScrapeOptions, FirecrawlApp
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class FireCrawlService:
+    def __init__(self):
+        api_key = os.getenv("firecrawl_api")
+        if not api_key:
+            raise ValueError("FireCrawl Api key not found")
+        self.app = FirecrawlApp(api_key=api_key)
+
+    def search_companies(self, query: str, num_results: int = 5):
+        try:
+            result = self.app.search(
+                query=f"",
+                limit=num_results,
+                scrape_options=ScrapeOptions(formats=["markdown"]),
+            )
+            return result
+        except Exception as e:
+            print(e)
+            return []
+
+    def scrape_company_pages(self, url: str):
+        try:
+            result = self.app.scrape_url(url, formats=["markdown"])
+            return result
+
+        except Exception as e:
+            print(e)
+            return None
---
+++ src/models.py
@@ -0,0 +1,3 @@
+from typing import List,Optional, Dict, Any
+from pydantic import BaseModel
+
